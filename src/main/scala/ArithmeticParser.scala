dd1467392b271e9935b3721ab8be99731aa4d938
Ram Narayanan <knram06@gmail.com>
2022-04-18 14:18:46 -0700

    Parse and evaluate multiple operators

--- src/main/scala/ArithmeticParser.scala
+++ src/main/scala/ArithmeticParser.scala
@@ -1,6 +1,3 @@
-import Op.Op
-
-import scala.util.matching.Regex
 import scala.util.parsing.combinator.RegexParsers
 import scala.util.parsing.combinator.PackratParsers
 
@@ -11,39 +8,55 @@ object Op extends Enumeration {
   val Add, Sub, Mult, Div, Val = Value
 }
 
-abstract class OpNode(var op: Op = Op.Val, var value: Int = 0) {
+abstract class OpNode {
   def getValue: Int
 }
 
-case class Operator(arg1: Number, arg2: Number) extends OpNode {
-  println("inside operator")
+case class Operator(oper: String, left: OpNode, right: OpNode) extends OpNode {
+  // println("inside operator")
 
   override def getValue: Int = {
-    2
+    val lv = left.getValue
+    val rv = right.getValue
+
+    oper match {
+      case "+" => lv + rv
+      case "-" => lv - rv
+      case "*" => lv * rv
+      case "/" => lv / rv
+      case _ =>
+        println("Unrecognized operator")
+        throw new Exception
+    }
   }
 }
 
 case class Number(num: Int) extends OpNode {
-  println("inside operand: " + num)
+  // println("inside operand: " + num)
 
   override def getValue: Int = {
-    5
+    num
   }
 }
 
 object ArithmeticParser extends RegexParsers with PackratParsers {
   // define lazy vals for what you want to parse
-  lazy val op: Regex = "[*+-/]".r
+  lazy val op: PackratParser[String] = "[*+-/]".r ^^ {s => s}
 
-  lazy val expr: PackratParser[OpNode] = (number <~ op) ~ number ^^ {case a ~ b => Operator(a, b)}
-
-  lazy val integer: PackratParser[OpNode] = number
+  lazy val expr: PackratParser[OpNode] = (
+    "(" ~ expr ~ op ~ expr ~ ")" ^^ {case s ~ a ~ b ~ c ~ t => Operator(b, a, c)} |
+    (expr ~ op ~ expr) ^^ {case a ~ b ~ c => Operator(b, a, c)} |
+    number
+    )
 
   lazy val number: PackratParser[Number] =
     "[0-9]+".r ^^ { n => Number(Integer.valueOf(n)) }
 
   def main(args: Array[String]): Unit = {
     // Execution
-    println(ArithmeticParser.parseAll(expr, "10+12"))
+    val parsedObj = ArithmeticParser.parseAll(expr, "10+12/15")
+    println(parsedObj)
+    val opNode = parsedObj.get
+    println("opTree value: " + opNode.getValue)
   }
 }
